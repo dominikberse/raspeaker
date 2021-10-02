5df8d5c0b0be6add923b59c8f67036162fe99a35
Dominik Berse <dominik.berse@tapdo.io>
2021-10-02 15:12:25 +0200

    feat: AltecLansing controller

--- modules/AltecLansing/controller.py
+++ modules/AltecLansing/controller.py
@@ -3,10 +3,33 @@ from core.module import ConsumingModule
 from core import pins
 
 import logging
+import pigpio
 
 
 class Controller(ConsumingModule):
-    """ Provides I2C communication with the AltecLansing main unit """
+    """ Injects I2C communication between the AltecLansing main unit and satellite """
+
+    I2C_ADDRESS = 0x45
 
     def __init__(self, pi, state, queue, app, config):
-        pass
+        super().__init__(pi, state, queue, app, config)
+        self._bus = config.require('i2c').value
+
+        # register as I2C slave
+        self._event = pi.event_callback(pigpio.EVENT_BSC, self._receive)
+        self._pi.bsc_i2c(Controller.I2C_ADDRESS)
+
+        # register as I2C master
+        self._i2c = self._pi.i2c_open(self._bus, Controller.I2C_ADDRESS)
+
+    def _receive(self, id, tick):
+        """ Receive command from satellite """
+
+        _, count, data = self._pi.bsc_i2c(Controller.I2C_ADDRESS)
+
+        # parse command to update internal state
+        if count == 2:
+            pass
+
+        # forward command to main unit
+        self._pi.i2c_write_device(self._i2c, data)
